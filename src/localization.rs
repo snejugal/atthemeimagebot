fc547da8d2727e4eccd90820181c2939e3f34010
SnejUgal <contact@snejugal.ru>
2019-10-27 22:12:02 +0700

    Move to async/await

--- src/localization.rs
+++ src/localization.rs
@@ -1,6 +1,6 @@
 use tbot::types::User;
 
-fn get_language(user: &Option<User>) -> Option<&str> {
+fn get_language(user: Option<&User>) -> Option<&str> {
     match user {
         Some(User {
             language_code: Some(language),
@@ -10,7 +10,7 @@ fn get_language(user: &Option<User>) -> Option<&str> {
     }
 }
 
-pub fn help_message(user: &Option<User>) -> &'static str {
+pub fn help_message(user: Option<&User>) -> &'static str {
     match get_language(user) {
         Some("ru") => "*Привет!* Я — бот, который может вытаскивать обои из .attheme файлов или ставить их. Чтобы вытащить обои, просто отправь тему. Если хотите поставить обои, то отправьте мне тему, а затем картинку (как фото или документом `jpg`, `png`, `bmp`, `tiff`, `webp`) ответом на эту тему. В ответ я отправлю тему с новыми обоями.",
         Some("fa") => "سلام! من یک ربات هستم که میتونم تصویر زمینه ی فایل های تم رو استخراج کنم و با یک تصویر زمینه ی جدید در تم ها قرار بدم. برای استخراج تصویر زمینه ی یک تم فقط کافیه اون تم رو به من ارسال کنی. اگر میخوای یک تصویر زمینه ی جدید در تم اعمال کنی، تم مورد نظرتو به من ارسال کن و بعدش در پاسخ به تم ارسالی تصویر زمینه ی مورد نظرتو ارسال کن. یادت نره فرمت مجاز تصاویر `jpg` و `png` و `bmp` و `tiff` و `webp` هستش. بعدش من تصویر زمینه ی قدیمی تم رو حذف و سپس تصویر زمینه مورد نظر شمارو در تم قرار میدم و ارسال میکنم.",
@@ -18,7 +18,7 @@ pub fn help_message(user: &Option<User>) -> &'static str {
     }
 }
 
-pub fn image_caption(user: &Option<User>) -> &'static str {
+pub fn image_caption(user: Option<&User>) -> &'static str {
     match get_language(user) {
         Some("ru") => {
             "Вот обои!\n\n\
@@ -38,7 +38,7 @@ pub fn image_caption(user: &Option<User>) -> &'static str {
     }
 }
 
-pub fn image_file_name(user: &Option<User>, theme_name: String) -> String {
+pub fn image_file_name(user: Option<&User>, theme_name: &str) -> String {
     match get_language(user) {
         Some("ru") => format!("Обои из {}.jpg", theme_name),
         Some("fa") => format!("تصویر زمینه {}.jpg", theme_name),
@@ -46,7 +46,7 @@ pub fn image_file_name(user: &Option<User>, theme_name: String) -> String {
     }
 }
 
-pub fn image_with_no_reply(user: &Option<User>) -> &'static str {
+pub fn image_with_no_reply(user: Option<&User>) -> &'static str {
     match get_language(user) {
         Some("ru") => "Если ты хочешь поставить обои в теме, то картинку надо отправить ответом на тему.",
         Some("fa") => "اِهِم! اگر میخوای تصویر زمینه جدید در تم قرار بدی، باید تصویر زمینه رو در پاسخ به فایل تم ارسال کنی :)",
@@ -54,19 +54,17 @@ pub fn image_with_no_reply(user: &Option<User>) -> &'static str {
     }
 }
 
-pub fn no_theme_in_reply(user: &Option<User>) -> &'static str {
+pub fn no_theme_in_reply(user: Option<&User>) -> &'static str {
     match get_language(user) {
         Some("ru") => {
             "Хм, кажется, что в сообщении, на которое ты ответил, нет темы. Попробуй снова."
         }
-        Some("fa") => {
-            "هومم، به نظر میرسه پیام شما در پاسخ به فایل تم نیست! دوباره امحتان کن."
-        }
+        Some("fa") => "هومم، به نظر میرسه پیام شما در پاسخ به فایل تم نیست! دوباره امحتان کن.",
         Some("en") | _ => "Hmm, doesn't seem the message you replied to has a theme. Try again.",
     }
 }
 
-pub fn start_message(user: &Option<User>) -> &'static str {
+pub fn start_message(user: Option<&User>) -> &'static str {
     match get_language(user) {
         Some("ru") => "Привет! Я — бот, который может вытаскивать обои из тем для Telegram на Android или ставить их. Просто отправь тему, а я отправлю обои.",
         Some("fa") => "سلام! من یک ربات هستم که میتونم تصاویر رو در تم های اندروید تلگرام استخراج کنم و یا تصویر زمینه ای رو در تم ها قرار بدم. فقط کافیه یک تم به من ارسال کنی، بعدش من تصویر زمینه ی تم رو بهتون میفرستم.",
@@ -74,17 +72,15 @@ pub fn start_message(user: &Option<User>) -> &'static str {
     }
 }
 
-pub fn theme_caption(user: &Option<User>) -> &'static str {
+pub fn theme_caption(user: Option<&User>) -> &'static str {
     match get_language(user) {
         Some("ru") => "Классные обои, поставил их в тему!",
-        Some("fa") => {
-            "تصویر زمینه مناسبیه! من این تصویر زمینه رو در تم قرار دادم!"
-        }
+        Some("fa") => "تصویر زمینه مناسبیه! من این تصویر زمینه رو در تم قرار دادم!",
         Some("en") | _ => "Nice wallpaper, I've added it to the theme!",
     }
 }
 
-pub fn theme_with_no_image(user: &Option<User>) -> &'static str {
+pub fn theme_with_no_image(user: Option<&User>) -> &'static str {
     match get_language(user) {
         Some("ru") => "Хм, кажется, что в этой теме нет обоев. Но я могу поставить обои в неё! Просто отправь сообщение ответом на эту тему.",
         Some("fa") => "عجب! به نظر میرسه تم ارسالی تصویر زمینه ای نداره. ولی نگران نباش! من میتونم یک تصویر زمینه بهش اضافه کنم. کافیه یک تصویر زمینه در پاسخ به این تم به من ارسال کنی.",
@@ -92,7 +88,7 @@ pub fn theme_with_no_image(user: &Option<User>) -> &'static str {
     }
 }
 
-pub fn unknown_file_extension(user: &Option<User>) -> &'static str {
+pub fn unknown_file_extension(user: Option<&User>) -> &'static str {
     match get_language(user) {
         Some("ru") => "Хм, я не знаю такую такое расширение темы или картинки. У тем должно быть расширение `.attheme`, у картинок — `jpg`, `png`, `bmp`, `tiff` или `webp`.",
         Some("fa") => "من بعضی تمها یا فایلها و تصاویر رو نمیشناسم! فرمت فایلهای تم .attheme هست. فرمت تصاویر هم `jpg` و `png` و `bmp` و `tiff` و `webp` میتونم قبول کنم.",
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 mod localization;
 
 use attheme::{Attheme, ColorSignature::Hex};
+use futures_util::try_join;
 use image::ImageOutputFormat::JPEG;
-use localization::*;
 use std::{path::Path, sync::Arc};
 use tbot::{
     connectors::Https,
-    contexts,
+    contexts::{
+        self,
+        fields::{MediaMessage, Message},
+    },
     prelude::*,
     types::{file::id::AsFileId, input_file, message, parameters::Text, Document},
     Bot,
@@ -14,249 +17,184 @@ use tbot::{
 
 const SUPPORTED_EXTENSIONS: [&str; 6] = ["png", "jpg", "jpeg", "bmp", "tiff", "webp"];
 
-fn download_document(
-    bot: &Arc<Bot<Https>>,
-    document: &impl AsFileId,
-) -> impl Future<Item = Vec<u8>, Error = ()> {
-    let bot = Arc::clone(bot);
+type Error = Box<dyn std::error::Error + Send + Sync + 'static>;
 
-    bot.get_file(document)
-        .into_future()
-        .map_err(|err| {
-            dbg!(err);
-        })
-        .and_then(move |file| {
-            bot.download_file(&file).map_err(|err| {
-                dbg!(err);
-            })
-        })
+async fn download(bot: &Bot<Https>, file: &impl AsFileId) -> Result<Vec<u8>, Error> {
+    let file = bot.get_file(file).call().await?;
+    Ok(bot.download_file(&file).await?)
 }
 
-macro_rules! set_wallpaper {
-    ($context:ident, $image:ident, $theme:ident) => {
-        let bot = Arc::clone(&$context.bot);
-        let chat_id = $context.chat.id;
-        let reply_id = $context.message_id;
-        let theme_name = match &$theme.file_name {
-            Some(filename) => filename.clone(),
-            None => return,
-        };
-        let theme_caption = Text::markdown(&theme_caption(&$context.from));
-
-        let download_image = download_document(&$context.bot, $image);
-        let download_theme = download_document(&$context.bot, $theme);
-
-        let download = download_image
-            .join(download_theme)
-            .map(move |(image, theme)| {
-                let mut theme = Attheme::from_bytes(&theme);
-
-                let image = match image::load_from_memory(&image) {
-                    Ok(image) => image,
-                    Err(err) => {
-                        dbg!(err);
-                        return;
-                    }
-                };
-
-                let mut wallpaper = Vec::new();
-
-                if let Err(error) = image.write_to(&mut wallpaper, JPEG(255)) {
-                    eprintln!("Error while writing to wallpaper: {:#?}", error);
-                    return;
-                }
-
-                theme.variables.remove("chat_wallpaper");
-                theme.wallpaper = Some(wallpaper);
-
-                let reply = bot
-                    .send_document(
-                        chat_id,
-                        input_file::Document::bytes(&theme_name, &theme.to_bytes(Hex))
-                            .caption(theme_caption),
-                    )
-                    .reply_to_message_id(reply_id)
-                    .into_future()
-                    .map_err(|err| {
-                        dbg!(err);
-                    });
-
-                tbot::spawn(reply);
-            });
-
-        tbot::spawn(download);
+async fn extract_wallpaper(context: &contexts::Document<Https>) -> Result<(), Error> {
+    let theme_name = match &context.document.file_name {
+        Some(theme_name) => theme_name,
+        None => Err("No name in document")?,
     };
+
+    let bytes = download(&context.bot, &context.document).await?;
+    let theme = Attheme::from_bytes(&bytes);
+
+    if let Some(image) = theme.wallpaper {
+        let file_name = localization::image_file_name(context.from.as_ref(), theme_name);
+        let caption = Text::markdown(localization::image_caption(context.from.as_ref()));
+        let wallpaper = input_file::Document::bytes(&file_name, &image).caption(caption);
+
+        context.send_document_in_reply(wallpaper).call().await?;
+    } else {
+        let error_text = Text::markdown(localization::theme_with_no_image(context.from.as_ref()));
+        context.send_message_in_reply(error_text).call().await?;
+    }
+
+    Ok(())
 }
 
-macro_rules! get_document {
-    ($context: ident) => {{
-        let context = $context;
-        let reply_to = match &context.reply_to {
-            Some(reply_to) => reply_to,
-            None => {
-                let reply = context
-                    .send_message_in_reply(Text::markdown(image_with_no_reply(&context.from)))
-                    .into_future()
-                    .map_err(|err| {
-                        dbg!(err);
-                    });
+async fn no_theme_in_reply(context: &impl Message<Https>) {
+    let error_text = Text::markdown(localization::no_theme_in_reply(context.from()));
+    let result = context.send_message_in_reply(error_text).call().await;
+    if let Err(err) = result {
+        dbg!(err);
+    }
+}
 
-                tbot::spawn(reply);
-                return;
+async fn get_document(context: &impl MediaMessage<Https>) -> Option<&Document> {
+    let reply_to = match context.reply_to() {
+        Some(reply_to) => reply_to,
+        None => {
+            let error_text = Text::markdown(localization::image_with_no_reply(context.from()));
+            let result = context.send_message_in_reply(error_text).call().await;
+            if let Err(err) = result {
+                dbg!(err);
             }
-        };
-
-        let no_theme_in_reply = || {
-            let reply = context
-                .send_message_in_reply(Text::markdown(no_theme_in_reply(&context.from)))
-                .into_future()
-                .map_err(|err| {
-                    dbg!(err);
-                });
-
-            tbot::spawn(reply);
-        };
-
-        let document = match &reply_to.kind {
-            message::Kind::Document(document, ..) => document,
-            _ => {
-                no_theme_in_reply();
-                return;
-            }
-        };
-
-        let filename = match &document.file_name {
-            Some(filename) => filename,
-            None => return,
-        };
-
-        let path = Path::new(&filename);
-        let extension: String = match path.extension() {
-            Some(extension) => extension.to_string_lossy().to_string(),
-            None => "".into(),
-        };
-
-        if extension != "attheme" {
-            no_theme_in_reply();
-            return;
+            return None;
         }
+    };
 
-        document
-    }};
+    let document = match &reply_to.kind {
+        message::Kind::Document(document, ..) => document,
+        _ => {
+            no_theme_in_reply(context).await;
+            return None;
+        }
+    };
+
+    let file_name = document.file_name.as_ref()?;
+    if Path::new(&file_name).extension().map(|x| x == "attheme") == Some(true) {
+        Some(document)
+    } else {
+        no_theme_in_reply(context).await;
+        None
+    }
 }
 
-fn extract_wallpaper(context: &contexts::Document<Https>, document: &Document) {
-    let bot = Arc::clone(&context.bot);
-    let chat_id = context.chat.id;
-    let reply_id = context.message_id;
-    let theme_name = match &document.file_name {
-        Some(filename) => filename.clone(),
+async fn set_wallpaper<C, I>(context: &C, image: &I, theme: &Document) -> Result<(), Error>
+where
+    C: Message<Https>,
+    I: AsFileId,
+{
+    let name = match &theme.file_name {
+        Some(name) => name,
+        None => Err("Document without a name")?,
+    };
+
+    let (image, theme) = try_join!(
+        download(context.bot(), image),
+        download(context.bot(), theme),
+    )?;
+
+    let mut theme = Attheme::from_bytes(&theme[..]);
+    let image = image::load_from_memory(&image[..])?;
+
+    let mut wallpaper = Vec::new();
+    image.write_to(&mut wallpaper, JPEG(255))?;
+
+    theme.variables.remove("chat_wallpaper");
+    theme.wallpaper = Some(wallpaper);
+
+    let caption = Text::markdown(&localization::theme_caption(context.from()));
+    let bytes = theme.to_bytes(Hex);
+    let new_theme = input_file::Document::bytes(&name, &bytes).caption(caption);
+    context.send_document_in_reply(new_theme).call().await?;
+
+    Ok(())
+}
+
+async fn start(context: Arc<contexts::Text<Https>>) {
+    let message = Text::markdown(localization::start_message(context.from.as_ref()));
+    let result = context.send_message(message).call().await;
+    if let Err(err) = result {
+        dbg!(err);
+    }
+}
+
+async fn help(context: Arc<contexts::Text<Https>>) {
+    let message = Text::markdown(localization::help_message(context.from.as_ref()));
+    let result = context.send_message(message).call().await;
+    if let Err(err) = result {
+        dbg!(err);
+    }
+}
+
+async fn document(context: Arc<contexts::Document<Https>>) {
+    let file_name = match &context.document.file_name {
+        Some(name) => name,
         None => return,
     };
-    let image_file_name = image_file_name(&context.from, theme_name);
-    let image_caption = Text::markdown(image_caption(&context.from));
-    let theme_with_no_image = Text::markdown(theme_with_no_image(&context.from));
-
-    let document = download_document(&context.bot, document).map(move |bytes| {
-        let theme = Attheme::from_bytes(&bytes);
-
-        match theme.wallpaper {
-            Some(image) => {
-                let reply = bot
-                    .send_document(
-                        chat_id,
-                        input_file::Document::bytes(&image_file_name, &image)
-                            .caption(image_caption),
-                    )
-                    .reply_to_message_id(reply_id)
-                    .into_future()
-                    .map_err(|err| {
-                        dbg!(err);
-                    });
-
-                tbot::spawn(reply);
+    let path = Path::new(&file_name);
+    let extension = match path.extension() {
+        Some(extension) => extension.to_string_lossy().to_lowercase(),
+        None => {
+            let error_text =
+                Text::markdown(localization::unknown_file_extension(context.from.as_ref()));
+            let result = context.send_message_in_reply(error_text).call().await;
+            if let Err(err) = result {
+                dbg!(err);
             }
-            None => {
-                let reply = bot
-                    .send_message(chat_id, theme_with_no_image)
-                    .reply_to_message_id(reply_id)
-                    .into_future()
-                    .map_err(|err| {
-                        dbg!(err);
-                    });
+            return;
+        }
+    };
 
-                tbot::spawn(reply);
-            }
-        };
-    });
-
-    tbot::spawn(document);
-}
-
-fn main() {
-    let mut bot = tbot::bot!("BOT_TOKEN").event_loop();
-
-    bot.start(|context| {
-        let message = Text::markdown(start_message(&context.from));
-
-        let reply = context.send_message(message).into_future().map_err(|err| {
+    if extension == "attheme" {
+        let result = extract_wallpaper(&*context).await;
+        if let Err(err) = result {
             dbg!(err);
-        });
+        }
+        return;
+    }
 
-        tbot::spawn(reply);
-    });
-
-    bot.help(|context| {
-        let message = Text::markdown(help_message(&context.from));
-
-        let reply = context.send_message(message).into_future().map_err(|err| {
-            dbg!(err);
-        });
-
-        tbot::spawn(reply);
-    });
-
-    bot.document(|context| {
-        let filename = match &context.document.file_name {
-            Some(name) => name,
+    if SUPPORTED_EXTENSIONS.contains(&extension.as_str()) {
+        let document = match get_document(&*context).await {
+            Some(document) => document,
             None => return,
         };
 
-        let path = Path::new(&filename);
-        let extension: String = match path.extension() {
-            Some(extension) => extension.to_string_lossy().to_lowercase().to_string(),
-            None => "".into(),
-        };
-
-        if extension == "attheme" {
-            extract_wallpaper(&context, &context.document);
-            return;
+        let image = &context.document;
+        let result = set_wallpaper(&*context, image, document).await;
+        if let Err(err) = result {
+            dbg!(err);
         }
+    }
+}
 
-        if SUPPORTED_EXTENSIONS.contains(&extension.as_str()) {
-            let document = get_document!(context);
-            let image = &context.document;
+async fn photo(context: Arc<contexts::Photo<Https>>) {
+    let document = match get_document(&*context).await {
+        Some(document) => document,
+        None => return,
+    };
 
-            set_wallpaper!(context, image, document);
-            return;
-        }
+    let image = context.photo.last().unwrap();
+    let result = set_wallpaper(&*context, image, document).await;
+    if let Err(err) = result {
+        dbg!(err);
+    }
+}
 
-        let reply = context
-            .send_message_in_reply(Text::markdown(unknown_file_extension(&context.from)))
-            .into_future()
-            .map_err(|err| {
-                dbg!(err);
-            });
+#[tbot::main]
+async fn main() {
+    let mut bot = tbot::from_env!("BOT_TOKEN").event_loop();
 
-        tbot::spawn(reply);
-    });
+    bot.start(start);
+    bot.help(help);
+    bot.document(document);
+    bot.photo(photo);
 
-    bot.photo(|context| {
-        let document = get_document!(context);
-        let image = context.photo.last().unwrap();
-
-        set_wallpaper!(context, image, document);
-    });
-
-    bot.polling().start();
+    bot.polling().start().await.unwrap();
 }
